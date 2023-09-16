e50fa0cfd3a0c00de967f1fc310e6493d32fb730
Tanya <itkofficial07@gmail.com>
2023-09-16 12:10:13 +0530

    Modified

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['axios'], // Specify 'axios' as an external dependency
+      output: {
+        format: 'commonjs', // Set the output format to 'commonjs'
+      },
+    },
+  },
+});
